f168ae8da4a87de404349a01565b1936f80a6dc6
Ian Czekala <ipc5094@psu.edu>
2021-04-20 23:36:33 -0400

    produce mock dataset.

---
+++ products/ALMA-logo/simobserve_cube.py
@@ -0,0 +1,19 @@
+import numpy as np
+import visread
+import casatasks
+import os
+
+# Generate an mock measurement set of the spectral cube
+
+fits_path = "logo_cube.fits"
+
+casatasks.simobserve(
+    skymodel=fits_path,
+    hourangle="transit",
+    totaltime="3600s",
+    graphics="none",
+    thermalnoise="tsys-atm",
+    overwrite=True,
+    obsmode="int",  # interferometer
+    antennalist="alma.cycle7.7.cfg",
+)
